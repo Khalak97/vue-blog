afc15c3964a800af4b6741f782f1e5bd8fa55c27
Khalak97 <valodyakh@gmail.com>
2021-08-13 10:54:56 +0300

    change

--- src/components/directives/VIntersection.js
+++ src/components/directives/VIntersection.js
@@ -1,5 +1,18 @@
 export default {
-  mounted(el) {
-    console.log(el);
+  mounted(el, binding) {
+    // console.log(el, binding);
+    // load posts auto intersection observer API
+    const options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+    const callback = (entries) => {
+      if (entries[0].isIntersecting) {
+        binding.value();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(el);
   },
+  name: "intersection",
 };
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <my-input v-model="searchQuery" placeholder="Search..." />
+    <my-input v-focus v-model="searchQuery" placeholder="Search..." />
 
     <div class="app__btns">
       <my-button @click="showDialog">
@@ -34,7 +34,7 @@
     />
     <div v-else>Loading...</div>
 
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
   </div>
 </template>
 
@@ -130,17 +130,17 @@ export default {
     this.fetchPosts();
 
     //load posts auto intersection observer API
-    const options = {
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-    const callback = (entries) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts();
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
+    // const options = {
+    //   rootMargin: "0px",
+    //   threshold: 1.0,
+    // };
+    // const callback = (entries) => {
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePosts();
+    //   }
+    // };
+    // const observer = new IntersectionObserver(callback, options);
+    // observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
